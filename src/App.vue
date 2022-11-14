fade83afbb3ff5bf9b62a714074aa26b0c740ecf
X <koonx6520@gmail.com>
2022-11-14 19:17:41 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <nav class="navbar">
+      <RouterLink to='/'><a>Dog</a></RouterLink>
+      <RouterLink to='/about'><a>About</a></RouterLink>
+    </nav>
+  </header>
+  <main>
+    <RouterView />
+  </main>
+</template>
+
+<script>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+.navbar {
+  display: flex;
+  justify-content: center;
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-bottom: 1.2em;
+}
+
+a {
+  padding: 10px;
+  text-decoration: none;
+  color: rgb(81, 90, 110)
+}
+a.router-link-active {
+  text-decoration: underline;
+  padding: 10px;
+}
+
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,15 @@
+<template>
+    <h1>This is a random dog search app.</h1>
+</template>
+
+<script>
+export default {
+    name: 'About'
+}
+</script>
+
+<style>
+h1 {
+    text-align: center;
+}
+</style>
